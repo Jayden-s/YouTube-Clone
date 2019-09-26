19e4ed6c95cdd55c86a5ec786883f0fb4c75aea9
Jayden Smith <jaydens19@icloud.com>
2019-09-26 11:14:55 +1000

    init

---
+++ src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onFormSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="feild">
+            <label>
+              <h1>Video Search</h1>
+            </label>
+            <input
+              placeholder="search for videos!"
+              onChange={this.onInputChange}
+              type="text"
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
